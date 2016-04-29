cad1acc0bde3e2833f348cf263f079c885fcbc49
CsatiZoltan <csati_zoltan@freemail.hu>
2016-04-29 22:03:07 +0200

    The Linux version uses the sleep function function from unistd.h.

--- relaxEye.c
+++ relaxEye.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +10,6 @@ int main(int argc, char* argv[])
    else
       wait = atof(argv[1]);
    printf("Timing started.\n\n");
-   Sleep(wait*60*1000);
+   sleep(wait*60);
    printf("\aAnother period of %.1f minutes elapsed. Relax your eyes for 10 minutes!\n", wait);
 }
